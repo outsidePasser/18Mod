ff04087de0e6cd700a9070d84ad37084f090900f
Michael Alexander <outsidepasser@gmail.com>
2019-09-22 12:03:08 -0400

    Game Selection dropdown

--- src/main/kotlin/com/mod/controller/StartController.kt
+++ src/main/kotlin/com/mod/controller/StartController.kt
@@ -5,7 +5,12 @@ import tornadofx.*
 class StartController : Controller() {
     // TODO: Get these from config file
     val gameOptions = arrayListOf("1830", "1822")
+    val minNumPlayers = 2
+    val maxNumPlayers = 7
 
+    fun setGame(game :String?) {
+        println("Game set to: $game")
+    }
 
 }
 
--- src/main/kotlin/com/mod/view/StartView.kt
+++ src/main/kotlin/com/mod/view/StartView.kt
@@ -2,21 +2,27 @@ package com.mod.view
 
 import com.mod.app.Styles
 import com.mod.controller.*
+import javafx.beans.property.SimpleStringProperty
 import tornadofx.*
 import javafx.collections.FXCollections
 
 class StartView : View() {
     val controller: StartController by inject()
     val gameOptions = FXCollections.observableArrayList(controller.gameOptions)
+    var selectedGame = SimpleStringProperty()
     val playerOptions = FXCollections.observableArrayList("2", "3", "4", "5", "6", "7")
+
+    init {
+        selectedGame.onChange { controller.setGame(it) }
+    }
 
     override val root = vbox() {
         label("18Mod") {
             addClass(Styles.heading)
         }
         label("Game") {}
-        combobox<String> {
-            items = gameOptions
+        combobox(selectedGame, gameOptions) {
+            selectionModel.select(0)
         }
         label("Number of Players") {}
         combobox<String> { items = playerOptions }
